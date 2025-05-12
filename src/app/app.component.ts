6d66bd7c2519beb0297ebd025081405a0e532636
rohan <rohannautiyal122000@gmail.com>
2025-05-13 00:16:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'RxJS';
+  users = [
+    {id: '1', name: 'Rohan', isActive: true},
+    {id: '2', name: 'Ryan', isActive: true},
+    {id: '3', name: 'Ron', isActive: true},
+  ]
 }
