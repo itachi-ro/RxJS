0e9b1ddc5374825b0cd9674dec4d76da18c66673
rohan <rohannautiyal122000@gmail.com>
2025-05-13 00:34:00 +0530

    users$

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,5 +14,8 @@ export class AppComponent {
     {id: '1', name: 'Rohan', isActive: true},
     {id: '2', name: 'Ryan', isActive: true},
     {id: '3', name: 'Ron', isActive: true},
-  ]
+  ];
+
+  users$ = of(this.users);
+  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)))
 }
